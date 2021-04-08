991f0ead4b8e3c73d036f92540340abd355ea3e7
Henrique Simoes <simoes.hmj@gmail.com>
2021-04-07 22:35:33 -0400

    feat: creat snake motion function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ snake[0] = {
   y: 8 * box,
 };
 
+let direction = "right";
+
 // função para criar um background
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -20,5 +22,26 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "down") snakeY += box;
+  if (direction == "up") snakeY -= box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
